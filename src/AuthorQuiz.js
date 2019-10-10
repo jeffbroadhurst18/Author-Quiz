bcf5394fe0b1f21817947cc21abf383f60eaedfe
jeffbroadhurst18 <jeffbroadhurst18@outlook.com>
2019-10-10 20:30:43 +0100

    Getting pictures to show

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React, { Component } from 'react';
 import './App.css';
 import './bootstrap.min.css';
@@ -11,8 +13,24 @@ const Hero = () => {
   </div>);
 };
 
-const Turn = () => {
-  return (<div />);
+const Book = ({title}) => {
+  return (<div className="answer">
+    <h4>{title}</h4>
+  </div>);
+}
+
+// map iterates through each element of an array
+const Turn = ({author, books}) => {
+  return (<div className="row turn" style={{backgroundColor: "white"}}>
+    <div className="col-4 offset-1">
+      <img src={author.imageUrl} className="authorimage" alt="Author" />
+    </div>
+    <div className="col-6"> 
+      {books.map(title => <Book title={title} key={title}/>)}
+    </div>
+  </div>
+  
+  );
 };
 
 const Continue = () => {
@@ -22,23 +40,23 @@ const Continue = () => {
 const Footer= () => {
   return (<div id="footer" className="row">
     <div className="col-12">
-      <p className="text-muted credit">All images are from <a href="http://commons.wikimedia.org/wiki">Wikimedia Commons</a></p>
+      <p className="text-muted credit">All images are from <a href="http://commons.wikimedia.org/wiki">Wikimedia Commons and are in the public domain</a></p>
     </div>
   </div>
   );
 };
 
-class AuthorQuiz extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <Hero />
-        <Turn />
-        <Continue />
-        <Footer />
-      </div>
-    );
-  }
+const AuthorQuiz=({turnData}) =>
+{
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData}/>
+      <Continue />
+      <Footer />
+    </div>
+  );
 }
+
 
 export default AuthorQuiz;
